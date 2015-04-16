ff1c8d9ee8e5c72f420c4cfba1fd0668aee8972b
it353project <it3530218@WEBDEV-33.ad.ilstu.edu>
2015-04-16 16:46:14 -0500

    Updated for DAO Changes

--- JavaGroupProject/src/java/dao/SearchDAOImpl.java
+++ JavaGroupProject/src/java/dao/SearchDAOImpl.java
@@ -26,30 +26,63 @@ public class SearchDAOImpl implements SearchDAO {
         /* Takes in the SearchBean, builds query. Depending on what the user has 
         filled in for search criteria, the query will change. Each field will 
         add an AND condition to the select statement, as each one should narrow
-        the possible parameters. 
+        the possible parameters. */
+        
+        String[] stringName = null;
+        String[] keywordHolder = null;
+        String keywords = null;
+        String firstName = null;
+        String lastName = null;
+        
+        String accountNameSearch = null;
+        String keywordSearch = null;
+        
+        /* Format Name into firstName, lastName, if AuthorName was used as a search criteria.
         The name field that's passed by SearchBean should be first parsed apart, first by ",",
         then by " ". If a comma is found, we'll assume that they've put lastname first, like 
         "Doe, John". If no comma is found, but a space is found, we'll assume "John Doe". If no
-        spaces or commas are found, we will assume it's a lastname, and omit firstname from the search.
-        Name will be done using a LIKE operator, as we're going to look for reasonable matches.
-        CourseNo will be an exact match, no frills. 
-        If keywords are present, it gets complex. You'll first need to look up the keywordID(s) from
-        the keyword table, then use that in a select statement from KeyAssign, obtaining all the 
-        ThesisIDs that have that/those keywordID(s). We will use this to create a view, which can then 
-        be searched using the rest of the criteria.
-        If a startdate is present, then search for dates greater than it. 
-        If an enddate is present, search for dates less than it.
-        */
+        spaces or commas are found, we will assume it's a lastname, and omit firstname from the search.*/
         
-        /*If there are keywords, create a view based on them. All searches from this point 
-        will be in that view. Otherwise, skip.*/
-        if (aSearch.getKeywords() != null){
-
+        if(aSearch.getAuthorName() != null){
+            stringName = aSearch.getAuthorName().split(",", 2);
+            if (stringName == null){
+                stringName = aSearch.getAuthorName().split(" ", 2);
+                if(stringName == null){
+                    lastName = aSearch.getAuthorName();
+                    firstName = null;
+                    accountNameSearch = "ACCOUNT.LASTNAME LIKE '%" + lastName +"%'";
+                }else{
+                    firstName = stringName[0];
+                    lastName = stringName[1];
+                    accountNameSearch = "ACCOUNT.FIRSTNAME LIKE '%" + firstName + "%' AND ACCOUNT.LASTNAME LIKE '%" + lastName +"%'";
+                }
+            }else{
+                lastName = stringName[0];
+                firstName = stringName[1];
+                accountNameSearch = "ACCOUNT.FIRSTNAME LIKE '%" + firstName + "%' AND ACCOUNT.LASTNAME LIKE '%" + lastName +"%'";
+            }
+        }
+        
+        /* Disassemble keywords, insert "'"s, reassemble, if there are keywords */
+        if(aSearch.getKeywords() != null){
+            keywordHolder = aSearch.getKeywords().split(",");
+            for(int i=0; i < keywordHolder.length; i++){
+                if (i == keywordHolder.length){
+                    /* If it's the last one, no comma at end. */
+                    keywords = keywords + "'" + keywordHolder[i] + "'";
+                }else{
+                    keywords = keywords + "'" + keywordHolder[i] + "',";
+                }        
+            }
             
         }
+        
+        
         /* Build the basic template for */
         String baseSelect = "SELECT * FROM ";
-        
+        String whereClause = "WHERE ("
+                + "ACCOUNT.FIRSTNAME LIKE '" + firstName + "'"
+                + "KEYWORD.KEYWORD IN (" + keywords + ")";
         /* Drop any views created */
         return null;
     }
