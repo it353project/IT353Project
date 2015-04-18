1f62d12a7fc6a09b52dbf2154d47fc077ef0cdd2
it353project <it3530218@WEBDEV-33.ad.ilstu.edu>
2015-04-18 12:13:29 -0500

    Updated fields, added SQL, tested.

--- JavaGroupProject/src/java/dao/SearchDAOImpl.java
+++ JavaGroupProject/src/java/dao/SearchDAOImpl.java
@@ -39,12 +39,13 @@ public class SearchDAOImpl implements SearchDAO {
         String keywordSearch = null;
         String query = null;
         
+        int clauseCounter = 0;
+        
         /* Format Name into firstName, lastName, if AuthorName was used as a search criteria.
         The name field that's passed by SearchBean should be first parsed apart, first by ",",
         then by " ". If a comma is found, we'll assume that they've put lastname first, like 
         "Doe, John". If no comma is found, but a space is found, we'll assume "John Doe". If no
         spaces or commas are found, we will assume it's a lastname, and omit firstname from the search.*/
-        
         if(aSearch.getAuthorName() != null){
             stringName = aSearch.getAuthorName().split(",", 2);
             if (stringName == null){
@@ -52,16 +53,21 @@ public class SearchDAOImpl implements SearchDAO {
                 if(stringName == null){
                     lastName = aSearch.getAuthorName();
                     firstName = null;
-                    accountNameSearch = "ACCOUNT.LASTNAME LIKE '%" + lastName +"%'";
+                    accountNameSearch = "IT353.ACCOUNT.LASTNAME LIKE '%" +
+                            lastName +"%'";
                 }else{
                     firstName = stringName[0];
                     lastName = stringName[1];
-                    accountNameSearch = "ACCOUNT.FIRSTNAME LIKE '%" + firstName + "%' AND ACCOUNT.LASTNAME LIKE '%" + lastName +"%'";
+                    accountNameSearch = "IT353.ACCOUNT.FIRSTNAME LIKE '%" +
+                            firstName + "%' AND IT353.ACCOUNT.LASTNAME LIKE '%" 
+                            + lastName +"%'";
                 }
             }else{
                 lastName = stringName[0];
                 firstName = stringName[1];
-                accountNameSearch = "ACCOUNT.FIRSTNAME LIKE '%" + firstName + "%' AND ACCOUNT.LASTNAME LIKE '%" + lastName +"%'";
+                accountNameSearch = "IT353.ACCOUNT.FIRSTNAME LIKE '%" + 
+                        firstName + "%' AND IT353.ACCOUNT.LASTNAME LIKE '%" +
+                        lastName +"%'";
             }
         }
         
@@ -76,13 +82,63 @@ public class SearchDAOImpl implements SearchDAO {
                     keywords = keywords + "'" + keywordHolder[i] + "',";
                 }        
             }
+            keywordSearch = "IT353.KEYWORD.KEYWORD IN (" + keywords + ")";
         }
           
         /* Build the basic template for */
-        String baseSelect = "SELECT * FROM ";
-        String whereClause = "WHERE ("
-                + "ACCOUNT.FIRSTNAME LIKE '" + firstName + "'"
-                + "KEYWORD.KEYWORD IN (" + keywords + ")";
+        String baseSelect = "SELECT IT353.ACCOUNT.FIRSTNAME, "
+                + "IT353.ACCOUNT.LASTNAME, IT353.THESIS.THESISNAME,"
+                + " IT353.THESIS.UPLOADDATE ";
+        String fromClause = "FROM IT353.ACCOUNT, IT353.THESIS ";
+        String joinClause = "JOIN IT353.THESIS ON IT353.THESIS.ACCOUNTID = IT353.ACCOUNT.ACCOUNTID" +
+            "JOIN IT353.KEYASSIGN ON IT353.KEYASSIGN.THESISID = IT353.THESIS.THESISID" +
+            "JOIN IT353.KEYWORD ON IT353.KEYWORD.KEYWORDID = IT353.KEYASSIGN.KEYWORDID";
+           
+        String whereClause = "WHERE ";
+        
+        /* Add author name to the WHERE clause if we have one */
+        if(accountNameSearch != null){
+            whereClause += accountNameSearch;
+            clauseCounter++;
+        }
+        
+        /* Adds the keyword search, if we've got one.If there has already been 
+        an addition to the WHERE clause, tack on an AND in front of this one. */
+        if(keywordSearch != null && clauseCounter > 0){
+            whereClause += keywordSearch;
+            clauseCounter++;
+        }else if(keywordSearch != null && clauseCounter == 0){
+            whereClause += " AND " + keywordSearch;
+            clauseCounter++;
+        }
+        
+        /* Adds the course number, if we've got one. If there has already been 
+        an addition to the WHERE clause, tack on an AND in front of this one. */
+        if (aSearch.getCourseNo() != null && clauseCounter == 0){
+            whereClause += "IT353.THESIS.COURSENO = " + aSearch.getCourseNo();
+            clauseCounter++;
+        }else if(aSearch.getCourseNo() != null){
+            whereClause += " AND IT353.THESIS.COURSENO = " + aSearch.getCourseNo();
+            clauseCounter++;
+        }
+        
+        /* Adds the date range, if we've got one. If there has already been 
+        an addition to the WHERE clause, tack on an AND in front of this one. */
+        if(aSearch.getStartDate() != null && clauseCounter > 0){
+            whereClause += " AND IT353.THESIS.UPLOADDATE >= " + aSearch.getStartDate();
+            clauseCounter++;            
+        }else if(aSearch.getStartDate() != null){
+            whereClause += "IT353.THESIS.UPLOADDATE >= " + aSearch.getStartDate();
+            clauseCounter++;            
+        }
+        
+        if(aSearch.getEndDate() != null && clauseCounter > 0){
+            whereClause += " AND IT353.THESIS.UPLOADDATE >= " + aSearch.getEndDate();
+            clauseCounter++;            
+        }else if(aSearch.getEndDate() != null){
+            whereClause += "IT353.THESIS.UPLOADDATE >= " + aSearch.getEndDate();
+            clauseCounter++;            
+        }
         
         /* Drop any views created */
         /* Pass the now-built query on to the database. */
