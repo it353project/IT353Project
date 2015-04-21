3774993d9362a54b0a95ba269d73d4e0f7794949
SilentArtifact <matt.gromer@gmail.com>
2015-04-21 07:31:32 -0500

    Creating ViewBean to hold end-user views.

---
+++ JavaGroupProject/src/java/model/ViewBean.java
@@ -0,0 +1,153 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package model;
+
+/**
+ *
+ * @author it3530229
+ */
+public class ViewBean {
+    private String thesisID;
+    private String authorName;
+    private String courseID;
+    private String[] keywords;
+    private String abstractCLOB;
+    private String attachmentLink;
+    private String screencastLink;
+    private String liveLink;
+    private String uploadDate;
+
+    /**
+     * @return the thesisID
+     */
+    public String getThesisID() {
+        return thesisID;
+    }
+
+    /**
+     * @param thesisID the thesisID to set
+     */
+    public void setThesisID(String thesisID) {
+        this.thesisID = thesisID;
+    }
+
+    /**
+     * @return the authorName
+     */
+    public String getAuthorName() {
+        return authorName;
+    }
+
+    /**
+     * @param authorName the authorName to set
+     */
+    public void setAuthorName(String authorName) {
+        this.authorName = authorName;
+    }
+
+    /**
+     * @return the courseID
+     */
+    public String getCourseID() {
+        return courseID;
+    }
+
+    /**
+     * @param courseID the courseID to set
+     */
+    public void setCourseID(String courseID) {
+        this.courseID = courseID;
+    }
+
+    /**
+     * @return the keywords
+     */
+    public String[] getKeywords() {
+        return keywords;
+    }
+
+    /**
+     * @param keywords the keywords to set
+     */
+    public void setKeywords(String[] keywords) {
+        this.keywords = keywords;
+    }
+
+    /**
+     * @return the abstractCLOB
+     */
+    public String getAbstractCLOB() {
+        return abstractCLOB;
+    }
+
+    /**
+     * @param abstractCLOB the abstractCLOB to set
+     */
+    public void setAbstractCLOB(String abstractCLOB) {
+        this.abstractCLOB = abstractCLOB;
+    }
+
+    /**
+     * @return the attachmentLink
+     */
+    public String getAttachmentLink() {
+        return attachmentLink;
+    }
+
+    /**
+     * @param attachmentLink the attachmentLink to set
+     */
+    public void setAttachmentLink(String attachmentLink) {
+        this.attachmentLink = attachmentLink;
+    }
+
+    /**
+     * @return the screencastLink
+     */
+    public String getScreencastLink() {
+        return screencastLink;
+    }
+
+    /**
+     * @param screencastLink the screencastLink to set
+     */
+    public void setScreencastLink(String screencastLink) {
+        this.screencastLink = screencastLink;
+    }
+
+    /**
+     * @return the liveLink
+     */
+    public String getLiveLink() {
+        return liveLink;
+    }
+
+    /**
+     * @param liveLink the liveLink to set
+     */
+    public void setLiveLink(String liveLink) {
+        this.liveLink = liveLink;
+    }
+
+    /**
+     * @return the uploadDate
+     */
+    public String getUploadDate() {
+        return uploadDate;
+    }
+
+    /**
+     * @param uploadDate the uploadDate to set
+     */
+    public void setUploadDate(String uploadDate) {
+        this.uploadDate = uploadDate;
+    }
+
+
+    
+    
+}
